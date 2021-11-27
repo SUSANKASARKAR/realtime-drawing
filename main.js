29a76ccd452ad6095d1e078c8067b900d1e37cc8
SUSANKASARKAR <89978984+SUSANKASARKAR@users.noreply.github.com>
2021-11-27 10:25:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.position(5,50);
+
+    canvas = createCanvas(400,400);
+    canvas.position(670,150);
+
+    poseNet = ml5.poseNet(video,modelDone);
+    poseNet.on('pose',gotposes);
+}
+
+function draw(){
+    background("#5196e3");
+}
+
+function modelDone(){
+    console.log("PoseNet Is Initialized And Loaded");
+}
+
+function gotposes(results,error){
+    if(error){
+        console.error(error);
+    }
+    if(results.length > 0){
+        console.log(results);
+
+        console.log("rightWrist_x = "+results[0].pose.rightWrist.x + " rightWrist_y = "+results[0].pose.rightWrist.y);
+        console.log("leftWrist_x = "+results[0].pose.leftWrist.x + " leftWrist_y = "+results[0].pose.leftWrist.y);
+    }
+}
